6d7bea500f725c9ba72e7b526177a2cde6079b75
Yohannes Getachew <demisgech@gmail.com>
2026-01-03 00:53:37 +0300

    Initial commit.

---
+++ android/app/src/main/kotlin/com/example/securepay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.securepay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/securepay_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SecurepayApp extends StatelessWidget {
+  const SecurepayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Center(child: Text('Securepay App'))),
+    );
+  }
+}
